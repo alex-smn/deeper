6c6d88811551b1219214cdb7f351aaf285d3cb9a
Alex <alexandr.i.semenov@gmail.com>
2024-01-29 18:09:59 +0200

    initial commit

---
+++ deeper-home-assignment/deeper_home_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  deeper_home_assignmentApp.swift
+//  deeper-home-assignment
+//
+//  Created by Alexander Livshits on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct deeper_home_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
